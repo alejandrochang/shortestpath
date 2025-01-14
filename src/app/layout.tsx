57f2e452f9bfcd83d3676539b6b6625f018c076a
Alejandro Chang <alejandrochang10@gmail.com>
2025-01-13 23:18:14 -0800

    fix: ga script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,23 +31,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
       <head>
         {/* Google Analytics Script */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-G7KZMBXJ3M`}
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', ${process.env.GOOGLE_ANALYTICS_ID}, {
-                page_path: window.location.pathname,
-              });
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G7KZMBXJ3M');
           `}
         </Script>
       </head>
